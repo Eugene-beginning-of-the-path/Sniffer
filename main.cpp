3165fece9ca5bcd95bd2b8f16175ab23ddf05232
EugenePC <EvgeniiDStudent@yandex.ru>
2023-03-27 23:55:31 +0300

    Created CMakeLists.txt and the first build of the project

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "stdlib.h"
+#include "PcapLiveDeviceList.h"
+#include "SystemUtils.h"
+
+/**
+* main method of the application
+*/
+int main(int argc, char* argv[])
+{
+    std::cout << "Success" << std::endl;
+}
