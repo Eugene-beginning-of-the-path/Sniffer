bbba5b79cc514bccc23d4744e6e53d261f4ce770
Eugene-beginning-of-the-path <EvgeniiDStudent@yandex.ru>
2023-03-29 03:08:22 +0300

    Write correct CMakeLitst.txt and successful compile the executable file

--- main.cpp
+++ main.cpp
@@ -3,10 +3,23 @@
 #include "PcapLiveDeviceList.h"
 #include "SystemUtils.h"
 
+
+
 /**
-* main method of the application
-*/
+ * main method of the application
+ */
 int main(int argc, char* argv[])
 {
-    std::cout << "Success" << std::endl;
+	// IPv4 address of the interface we want to sniff
+	std::string interfaceIPAddr = "127.0.0.1";
+
+	// find the interface by IP address
+	pcpp::PcapLiveDevice* dev = pcpp::PcapLiveDeviceList::getInstance().getPcapLiveDeviceByIp(interfaceIPAddr);
+	if (dev == NULL)
+	{
+		std::cerr << "Cannot find interface with IPv4 address of '" << interfaceIPAddr << "'" << std::endl;
+		return 1;
+	}
+
+    std::cout << "Full Successful" << std::endl;
 }
